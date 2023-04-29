8c9dc2c7b771d4b8ca5734781d60050cdaa7b8db
LadyMeow <72276947+LadyMeow@users.noreply.github.com>
2023-04-29 23:05:38 +0200

    fixes

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -26,30 +26,54 @@ public class DeliveryManager : MonoBehaviour
 
         // check Inputs
         // Input.GetMouseButtonDown(0)
-
+        
         //if (cat.Hunger < 100 && Money >= FoodCost)
         // {
         //     SpendMoney(FoodCost);
         // }
-
-
-
-
-
-
-
-
+        //if (cat.CurrentActivity == null)
+        //{
+        //    AddMoney(delivery.Payment);
+        //}
 
 
         // Loop do Cats
         foreach (Cat cat in MyCats)
         {
+            if (cat.CurrentActivity.Finished)
+            {
+                switch (cat.CurrentActivity)
+                {
+                    case Delivering:
+                        AddMoney(((Delivering)cat.CurrentActivity).Delivery.Payment);
+                        break;
+                    default:
+                        break;
+                }
+
+                cat.CurrentActivity = new Resting();
+            }
             cat.DoActivity();
         }
-
     }
 
-    
+    // BUTTONS - getcomponent verwenden?!
+    public void FeedCat(int cat)
+    {
+        if (MyCats[cat].CurrentActivity is Resting)
+        {
+            MyCats[cat].CurrentActivity = new GettingFood();
+        }
+    }
+
+    public void SendCatOnDelivery(int cat)
+    {
+        if (MyCats[cat].CurrentActivity is Resting)
+        {
+            MyCats[cat].CurrentActivity = new Delivering(SelectedDelivery);
+        }
+    }
+
     // MONEY
     public int Money = 100;
     public int FoodCost = 10;
@@ -77,15 +101,8 @@ public class DeliveryManager : MonoBehaviour
     public List<Delivery> ActiveDeliveries { get; set; }
     public List<Delivery> FinishedDeliveries { get; set; }
 
-    public void SendCatOnDelivery(Cat cat, Delivery delivery)
-    {
-        if (cat.CurrentActivity == null) 
-        {
-            AddMoney(delivery.Payment);
-        }
-    }
+    public Delivery SelectedDelivery { get; set; }
 
-    // method for finished delivery (addMoney etc)
 
 
     // BASE & CAT STATS
@@ -111,14 +128,8 @@ public class DeliveryManager : MonoBehaviour
         }
     }
 
-    public void FeedCat(Cat cat)
-    {
-
-    }
-
 
     // TIME MANAGEMENT
-    public Timer Timer;
 
     public void AfterFiveSeconds()
     {

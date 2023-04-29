66d3831257a2e166dd15e4211cdd082c5892e38b
LadyMeow <72276947+LadyMeow@users.noreply.github.com>
2023-04-29 18:38:45 +0200

    hunger and feeding added

--- Assets/Scripts/Activities/GettingFood.cs
+++ Assets/Scripts/Activities/GettingFood.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Feeding : Activity
+public class GettingFood : Activity
 {
     public int Cost { get; set; }
 
--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,9 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
+    // MONEY
+    public int Money = 100;
+    public int FoodCost = 10;
+
     void Start()
     {
 
@@ -11,7 +16,7 @@ public class DeliveryManager : MonoBehaviour
         Cat starterCat1 = new Cat("Gin", "Gin is very lively and loves to play with other cats.", Sprites[0], 2000, 100);
         Cat starterCat2 = new Cat("Tonic", "Tonic is a little shy with strangest but loves to cuddle.", Sprites[1], 2000, 100);
         MyCats = new List<Cat>();
-        MyCats.Add(starterCat1); 
+        MyCats.Add(starterCat1);
         MyCats.Add(starterCat2);
 
     }
@@ -36,13 +41,15 @@ public class DeliveryManager : MonoBehaviour
 
 
 
-    // BASE
+    // BASE & CAT STATS
     // passive Energy gain when Cats are in Base
 
-    public void energyManagement()
+    public void StatusManagement()
     {
-        foreach (Cat cat in MyCats) 
+        foreach (Cat cat in MyCats)
         {
+            cat.Hunger -= 1;
+
             if (cat.CurrentActivity == null)
             {
                 // Upgrades for Base will give more recreation for energy
@@ -56,6 +63,28 @@ public class DeliveryManager : MonoBehaviour
         }
     }
 
+    public void FeedCat(Cat cat)
+    {
+        // costs something
+        // can only be clicked if cat.Hunger < 100 && Money > 10
+        if (cat.Hunger < 100 && Money >= FoodCost)
+        {
+            float time = 0.0f;
+            float interpolationPeriod = 1;
+
+            while (cat.Hunger < 100)
+            {
+                time += Time.deltaTime;
+
+                if (time >= interpolationPeriod)
+                {
+                    time = 0.0f;
+
+                    cat.Hunger += 2;
+                }
+            }
+        }
+    }
 
 
     // TIME MANAGEMENT
@@ -65,8 +94,9 @@ public class DeliveryManager : MonoBehaviour
     {
         Debug.Log("interpolating");
 
-        energyManagement();
+        StatusManagement();
     }
+
 
     // SHELTER
     public Shelter Shelter;

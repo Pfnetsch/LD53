fbff58a04c300df02647466312ea45b8fb1d1463
Patrick Seebacher <patrick.seebacher@gmail.com>
2023-04-30 22:29:47 +0200

    UI Progress Bar Fuckery

--- Assets/Scripts/UI/CatListEntryController.cs
+++ Assets/Scripts/UI/CatListEntryController.cs
@@ -5,14 +5,24 @@ using UnityEngine.UIElements;
 public class CatListEntryController
 {
     private VisualElement _root;
+
     private Label _catNameLabel;
-    private Label _catInfoLabel;
+
 
     public void SetVisualElement(VisualElement visualElement)
     {
         _root = visualElement;
         _catNameLabel = _root.Q<Label>("CatName");
-        _catInfoLabel = _root.Q<Label>("CatInfo");
+
+        foreach (var pb in _root.Query("unity-progress-bar").Build())
+        {
+            pb.style.marginBottom = 6;
+            pb.style.marginLeft = 3;
+            pb.style.marginRight = 3;
+        }
+
+        //.unity-progress-bar__progress {
+        //background - color: rgb(41, 159, 64);
     }
 
     public void SetCatInformation(Cat cat)
